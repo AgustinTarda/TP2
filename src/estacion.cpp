b3af7704ae091f02957e810455e3cbc7976e2eac
rochi <rochi@rochi-Inspiron-5379>
2019-10-11 16:41:20 -0300

    Cree los archivos de estacion

---
+++ src/estacion.cpp
@@ -0,0 +1,13 @@
+/*
+ * estacion.cpp
+ *
+ *  Created on: 11 oct. 2019
+ *      Author: rochi
+ */
+#include "estacion.h"
+
+class Estacion {
+
+	std::string nombre;
+
+};
---
+++ src/estacion.h
@@ -0,0 +1,13 @@
+/*
+ * estacion.h
+ *
+ *  Created on: 11 oct. 2019
+ *      Author: rochi
+ */
+
+#ifndef SRC_ESTACION_H_
+#define SRC_ESTACION_H_
+
+#include <string>
+
+#endif /* SRC_ESTACION_H_ */
