044bb4b4600f7b5294c9123746d486dde5b9732b
rochi <rochi@rochi-Inspiron-5379>
2019-10-17 13:21:49 -0300

    Cree los .h de metrous, subte y ferrocarril

---
+++ ferrocarril.h
@@ -0,0 +1,35 @@
+/*
+ * ferrocarril.h
+ *
+ *  Created on: 17 oct. 2019
+ *      Author: rochi
+ */
+
+#ifndef FERROCARRIL_H_
+#define FERROCARRIL_H_
+
+#include <string>
+
+class EstacionFerrocarril{
+
+private:
+	int longitud;
+	int latitud;
+	int id;
+	std:: string nombre;
+	std:: string linea;
+	std:: string linea2;
+	std:: string ramal;
+	std:: string barrio;
+	std:: string comuna;
+	std:: string localidad;
+	std:: string partido;
+
+
+
+
+};
+
+
+
+#endif /* FERROCARRIL_H_ */
