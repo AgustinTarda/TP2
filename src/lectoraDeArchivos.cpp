3327e72104a10b563b5b9b9c0b310ae1c99de249
agustin <agustin@agustin-HP-ENVY-x360-Convertible>
2019-10-18 15:04:29 -0300

    a

--- src/lectoraDeArchivos.cpp
+++ src/lectoraDeArchivos.cpp
@@ -1,8 +1,10 @@
 /*
- * garage.cpp
+ * lectoraDeArchivos.cpp
  *
  *  Created on: 17 oct. 2019
  *      Author: agustin
  */
-#include "garage.h"
+
+
+
 
---
+++ src/lectoraDeArchivos.h
@@ -0,0 +1,17 @@
+/*
+ * lectoraDeArchivos.h
+ *
+ *  Created on: 17 oct. 2019
+ *      Author: agustin
+ */
+
+#ifndef LECTORADEARCHIVOS_H_
+#define LECTORADEARCHIVOS_H_
+class lectoraDeArchivos{
+
+};
+
+
+
+
+#endif /* LECTORADEARCHIVOS_H_ */
