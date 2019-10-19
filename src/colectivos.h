4c3e1474f4d68d8e2128c3917689acedccab494d
rochi <rochi@rochi-Inspiron-5379>
2019-10-19 17:47:34 -0300

    Hice el leer archivos de ferrocarril y colectivos

--- src/colectivos.h
+++ src/colectivos.h
@@ -5,28 +5,35 @@
  *      Author: melanie
  */
 
-
 #ifndef SRC_COLECTIVOS_H_
 #define SRC_COLECTIVOS_H_
 
 #include <string>
 
-class EstacionColectivo{
-	private:
-		unsigned int id;
-		unsigned int codigo;
-		std:: string nombre;
-		int latitud;
-		int longitud;
-		unsigned int idDireccion;
-		unsigned int idRuta;
-		unsigned int idAgencia;
-		std:: string rutaNombreCorto;
-		std:: string rutaNombreLargo;
-		std:: string rutaDesc;
-		unsigned int tipoRuta;
+class EstacionColectivo {
+private:
+	unsigned int id;
+	unsigned int codigo;
+	std::string nombre;
+	double latitud;
+	double longitud;
+	unsigned int idDireccion;
+	unsigned int idRuta;
+	unsigned int idAgencia;
+	std::string rutaNombreCorto;
+	std::string rutaNombreLargo;
+	std::string rutaDesc;
+	unsigned int tipoRuta;
+
+public:
+	EstacionColectivo(unsigned int id, unsigned int codigo, std::string nombre,
+			double latitud, double longitud, unsigned int idDireccion,
+			unsigned int idRuta, unsigned int idAgencia,
+			std::string rutaNombreCorto, std::string rutaNombreLargo,
+			std::string rutaDesc, unsigned int tipoRuta);
+	EstacionColectivo(const EstacionColectivo &otraEstacion);
+	EstacionColectivo();
+
 };
-
-
 
 #endif /* SRC_COLECTIVOS_H_ */
