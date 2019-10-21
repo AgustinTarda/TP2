599b89466d2ebc7f105249ac3ce157ff5e6bbd16
rochi <rochi@rochi-Inspiron-5379>
2019-10-21 13:54:47 -0300

    Segui modificando la clase de Subte

--- src/colectivos.cpp
+++ src/colectivos.cpp
@@ -5,20 +5,21 @@
  *      Author: melanie
  */
 
-#include <string>
 #include "colectivos.h"
 
-EstacionColectivo::EstacionColectivo(unsigned int id, unsigned int codigo, std::string nombre,
-		double latitud, double longitud, unsigned int idDireccion,
-		unsigned int idRuta, unsigned int idAgencia,
+EstacionColectivo::EstacionColectivo(unsigned int id, unsigned int codigo,
+		std::string nombre, double latitud, double longitud,
+		unsigned int idDireccion, unsigned int idRuta, unsigned int idAgencia,
 		std::string rutaNombreCorto, std::string rutaNombreLargo,
-		std::string rutaDesc, unsigned int tipoRuta){
+		std::string rutaDesc, unsigned int tipoRuta) {
 
 	this->id = id;
 	this->codigo = codigo;
 	this->nombre = nombre;
 	this->latitud = latitud;
 	this->longitud = longitud;
+	this->idRuta = idRuta;
+	this->idAgencia = idAgencia;
 	this->idDireccion = idDireccion;
 	this->rutaNombreCorto = rutaNombreCorto;
 	this->rutaNombreLargo = rutaNombreLargo;
@@ -33,6 +34,8 @@ EstacionColectivo::EstacionColectivo(const EstacionColectivo &otraEstacion) {
 	this->nombre = otraEstacion.nombre;
 	this->latitud = otraEstacion.latitud;
 	this->longitud = otraEstacion.longitud;
+	this->idRuta = otraEstacion.idRuta;
+	this->idAgencia = otraEstacion.idAgencia;
 	this->idDireccion = otraEstacion.idDireccion;
 	this->rutaNombreCorto = otraEstacion.rutaNombreCorto;
 	this->rutaNombreLargo = otraEstacion.rutaNombreLargo;
